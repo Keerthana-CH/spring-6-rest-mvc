e61f0b01323ffef196be34f9a4bbdcb48276130b
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-21 08:49:39 -0700

    @AllArgsController of Lombok

---
+++ src/main/java/com/sfg/spring6restmvc/controller/BeerController.java
@@ -0,0 +1,11 @@
+package com.sfg.spring6restmvc.controller;
+
+import com.sfg.spring6restmvc.services.BeerService;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Controller;
+
+@AllArgsConstructor
+@Controller
+public class BeerController {
+    private final BeerService beerService;
+}
