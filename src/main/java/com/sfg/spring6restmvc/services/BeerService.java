13044092ab78b4a39586f8abea77996a8093379a
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-21 10:52:49 -0700

    /api/v1/beer

--- src/main/java/com/sfg/spring6restmvc/services/BeerService.java
+++ src/main/java/com/sfg/spring6restmvc/services/BeerService.java
@@ -2,8 +2,11 @@ package com.sfg.spring6restmvc.services;
 
 import com.sfg.spring6restmvc.model.Beer;
 
+import java.util.List;
 import java.util.UUID;
 
 public interface BeerService {
     public Beer getBeerById(UUID id);
+
+    List<Beer> listBeers();
 }
