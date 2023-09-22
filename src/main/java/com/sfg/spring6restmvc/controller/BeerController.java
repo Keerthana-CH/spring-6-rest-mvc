eda492208737cc8e36896b238d40f193827d3192
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-22 02:07:25 -0700

    dev-features

--- src/main/java/com/sfg/spring6restmvc/controller/BeerController.java
+++ src/main/java/com/sfg/spring6restmvc/controller/BeerController.java
@@ -27,7 +27,7 @@ public class BeerController {
     @RequestMapping(value ="/{beerId}" , method = RequestMethod.GET)
     public Beer getBeerById(@PathVariable("beerId") UUID beerId){
 
-        log.debug("getBeerById - controller BeerController"+beerId);
+        log.debug("getBeerById - ********* controller BeerController"+beerId);
         return beerService.getBeerById(beerId);
     }
 }
