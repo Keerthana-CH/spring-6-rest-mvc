143d2d1fdcb6934dc2b507c97be411ffebe2191a
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-25 05:37:23 -0700

    Object Mapper

--- src/test/java/com/sfg/spring6restmvc/controller/BeerControllerTest.java
+++ src/test/java/com/sfg/spring6restmvc/controller/BeerControllerTest.java
@@ -1,5 +1,7 @@
 package com.sfg.spring6restmvc.controller;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.sfg.spring6restmvc.model.Beer;
 import com.sfg.spring6restmvc.services.BeerService;
 import com.sfg.spring6restmvc.services.BeerServiceImpl;
@@ -31,7 +33,20 @@ class BeerControllerTest {
     @MockBean
     BeerService beerService;
 
+    @Autowired
+    ObjectMapper objectMapper;
+
     BeerServiceImpl beerServiceImpl = new BeerServiceImpl();
+
+    @Test
+    void testCreateNewBeer() throws JsonProcessingException {
+/*        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.findAndRegisterModules();*/
+
+        Beer beer = beerServiceImpl.listBeers().get(0);
+
+        System.out.println(objectMapper.writeValueAsString(beer));
+    }
 
     @Test
     void listBeers() throws Exception {
