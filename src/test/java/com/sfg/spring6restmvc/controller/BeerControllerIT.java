b14d53ad44b8b97d9dc362221422812cdcc02503
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-26 21:48:38 -0700

    BeerControllerIT

---
+++ src/test/java/com/sfg/spring6restmvc/controller/BeerControllerIT.java
@@ -0,0 +1,41 @@
+package com.sfg.spring6restmvc.controller;
+
+import com.sfg.spring6restmvc.model.BeerDTO;
+import com.sfg.spring6restmvc.repositories.BeerRepository;
+import jakarta.transaction.Transactional;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+class BeerControllerIT {
+
+    @Autowired
+    BeerController beerController;
+
+    @Autowired
+    BeerRepository beerRepository;
+
+    @Test
+    void testListBeers() {
+        List<BeerDTO> beerDTOS = beerController.listBeers();
+
+        assertThat(beerDTOS.size()).isEqualTo(3);
+    }
+
+    @Rollback
+    @Transactional
+    @Test
+    void testEmptyList() {
+        beerRepository.deleteAll();
+        List<BeerDTO> beerDTOS = beerController.listBeers();
+
+        assertThat(beerDTOS.size()).isEqualTo(0);
+    }
+}
