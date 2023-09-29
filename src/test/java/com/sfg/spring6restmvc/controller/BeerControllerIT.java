baf08c9ff50bf6b2b2caedca7c9300e9d7f9f72f
Keerthana-CH <110730952+Keerthana-CH@users.noreply.github.com>
2023-09-29 03:45:40 -0700

    test for not found exception

--- src/test/java/com/sfg/spring6restmvc/controller/BeerControllerIT.java
+++ src/test/java/com/sfg/spring6restmvc/controller/BeerControllerIT.java
@@ -1,5 +1,6 @@
 package com.sfg.spring6restmvc.controller;
 
+import com.sfg.spring6restmvc.entities.Beer;
 import com.sfg.spring6restmvc.model.BeerDTO;
 import com.sfg.spring6restmvc.repositories.BeerRepository;
 import jakarta.transaction.Transactional;
@@ -9,6 +10,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.annotation.Rollback;
 
 import java.util.List;
+import java.util.UUID;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,6 +23,22 @@ class BeerControllerIT {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testBeerIdNotFoundException() {
+        assertThrows(NotFoundException.class,() ->{
+            beerController.getBeerById(UUID.randomUUID());
+        });
+    }
+
+    @Test
+    void testGetById() {
+        Beer beer = beerRepository.findAll().get(0);
+
+        BeerDTO beerDTO = beerController.getBeerById(beer.getId());
+
+        assertThat(beerDTO).isNotNull();
+    }
 
     @Test
     void testListBeers() {
