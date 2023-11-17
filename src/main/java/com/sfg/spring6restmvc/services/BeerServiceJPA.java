1ffa58a99e068d4656fe43d5c22a0fe4bdb3a356
Keerthana-CH <chilupurikeerthana@gmail.com>
2023-11-17 12:55:50 -0800

    Find By Name with Spring Data JPA

--- src/main/java/com/sfg/spring6restmvc/services/BeerServiceJPA.java
+++ src/main/java/com/sfg/spring6restmvc/services/BeerServiceJPA.java
@@ -46,8 +46,7 @@ public class BeerServiceJPA implements BeerService {
     }
 
     private List<Beer> listBeersByBeerName(String beerName) {
-
-        return new ArrayList<>();
+        return beerRepository.findAllByBeerNameIsLikeIgnoreCase(beerName);
     }
 
     @Override
--- src/test/java/com/sfg/spring6restmvc/repositories/BeerRepositoryTest.java
+++ src/test/java/com/sfg/spring6restmvc/repositories/BeerRepositoryTest.java
@@ -1,21 +1,33 @@
 package com.sfg.spring6restmvc.repositories;
 
+import com.sfg.spring6restmvc.bootstrap.BoostrapData;
 import com.sfg.spring6restmvc.entities.Beer;
 import com.sfg.spring6restmvc.model.BeerStyle;
+import com.sfg.spring6restmvc.services.BeerCsvServiceImpl;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
 
 @DataJpaTest
+@Import({BoostrapData.class, BeerCsvServiceImpl.class})
 class BeerRepositoryTest {
 
     @Autowired
     BeerRepository beerRepository;
+
+    @Test
+    void testGetBeerListByName(){
+        List<Beer> allByBeerNameIsLikeIgnoreCase = beerRepository.findAllByBeerNameIsLikeIgnoreCase("%IPA");
+
+        assertThat(allByBeerNameIsLikeIgnoreCase.size()).isEqualTo(291);
+    }
 
     @Test
     void testSaveBeer() {
